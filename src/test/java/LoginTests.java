aa09c7324ff1d9024cf7a7a89071eae4b9f6e2d0
Supattra <supattra.tangsombutpaiboon@testpro.io>
2023-07-08 13:16:38 -0500

    homework-22
    Supattra-homework-22

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,16 +1,14 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends BaseTest {
-    @Test (dataProvider = "CorrectLoginProviders", dataProviderClass = BaseTest.class)
-    public void LoginEmptyEmailPasswordTest(String email, String password) {;
-        //provide email
-        enterEmail(email);
-        //provide password
-        enterPassword(password);
-        //click submit
-        clickSubmit();
+    @Test
+    public void LoginValidEmailPasswordTest() {
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        loginPage.login();
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 }
-
-
